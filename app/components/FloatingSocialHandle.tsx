838888f8af021b1bad9c50f8247fe8a9be2f362b
iamayushdas <ayush24das@gmail.com>
2023-12-25 21:36:15 +0530

    fix: small icon fix

--- app/components/FloatingSocialHandle.tsx
+++ app/components/FloatingSocialHandle.tsx
@@ -1,16 +1,39 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Button } from "@/components/ui/button";
 import SocialHandles from "./SocialHandles";
 import { MessagesSquare, X } from "lucide-react";
 
 const FloatingSocialHandle: React.FC = () => {
   const [socialHandlesVisible, setSocialHandlesVisible] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
   const toggleSocialHandles = () => {
     setSocialHandlesVisible(!socialHandlesVisible);
   };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      buttonRef.current &&
+      !buttonRef.current.contains(event.target as Node) &&
+      event.target !== buttonRef.current
+    ) {
+      setSocialHandlesVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    if (socialHandlesVisible) {
+      document.addEventListener("click", handleClickOutside);
+    } else {
+      document.removeEventListener("click", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [socialHandlesVisible]);
 
   return (
     <div>
@@ -21,11 +44,16 @@ const FloatingSocialHandle: React.FC = () => {
       )}
 
       <Button
+        ref={buttonRef}
         onClick={toggleSocialHandles}
         className="fixed bottom-8 bg-teal-100 left-4 z-10 text-white p-2 rounded-full cursor-pointer hover:bg-teal-500 shadow-lg"
         style={{ width: "50px", height: "50px" }}
       >
-        {socialHandlesVisible ? <X className="text-teal-500 hover:text-white w-8 h-8" /> : <MessagesSquare className="text-teal-500 hover:text-white w-8 h-8"/>}
+        {socialHandlesVisible ? (
+          <X className="text-teal-500 hover:text-white w-8 h-8" />
+        ) : (
+          <MessagesSquare className="text-teal-500 hover:text-white w-8 h-8" />
+        )}
       </Button>
     </div>
   );
