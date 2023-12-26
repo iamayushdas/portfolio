6311e1253d23ba13f921a589ff4c23f56c3813d9
iamayushdas <ayush24das@gmail.com>
2023-12-26 13:04:40 +0530

    feat: /studio /contentful added

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["cdn.sanity.io"]
-    }
-}
+  images: {
+    domains: ["cdn.sanity.io"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/studio",
+        destination: "https://portfolio-iamayushdas.sanity.studio",
+        permanent: false,
+      },
+      {
+        source: "/contentful",
+        destination:
+          "https://app.contentful.com/spaces/i17fgav06ug6/entries/4Yq1AhsNeSl6vv3FIvN4X0",
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
