d69006fc252bf488bbbd5697f2de107c34e6cd09
iamayushdas <ayush24das@gmail.com>
2023-12-26 11:52:09 +0530

    feat: /studio removed

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
-// next.config.js
-module.exports = {
-  images: {
-    domains: ["cdn.sanity.io"],
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/studio/:path*",
-        destination: "https://xrt5fta3.sanity.studio/:path*",
-      },
-    ];
-  },
-};
+const nextConfig = {
+    images: {
+        domains: ["cdn.sanity.io"]
+    }
+}
+
+module.exports = nextConfig
